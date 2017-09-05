364e3ed5eedba21294cc9c8dfc18939601f614b2
TaimoorTahir <bitf14a040@pucit.edu.pk>
2017-09-05 21:09:25 +0500

    Adapter set Working on recycler view

---
+++ app/src/main/java/com/example/taimoortahir/soraproject/Image.java
@@ -0,0 +1,24 @@
+package com.example.taimoortahir.soraproject;
+
+/**
+ * Created by TaimoorTahir on 05/09/2017.
+ */
+
+public class Image {
+    String image;
+
+    public Image(){
+    }
+
+    public Image(String image) {
+        this.image = image;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
--- app/src/main/java/com/example/taimoortahir/soraproject/ImageAdapter.java
+++ app/src/main/java/com/example/taimoortahir/soraproject/ImageAdapter.java
@@ -1,70 +1,109 @@
+package com.example.taimoortahir.soraproject;
 
-
+import android.content.Context;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
-
-import com.example.taimoortahir.soraproject.R;
+import android.widget.Toast;
 
 import java.util.List;
 
 /**
- * Created by Taimoor Tahir on 23-Jul-17.
+ * Created by TaimoorTahir on 30/08/2017.
  */
 
-public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.MyViewHolder> {
+public class AlbumsAdapter extends RecyclerView.Adapter<AlbumsAdapter.MyViewHolder> {
 
-    private List<Task> taskList;
-
-    private onBack ob;
+    private Context mContext;
+    private List<Image> imageList;
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
-        public TextView task, date;
+        public TextView title, count;
+        public ImageView thumbnail, overflow;
 
         public MyViewHolder(View view) {
             super(view);
-            task = (TextView) view.findViewById(R.id.task_txt);
-            date = (TextView) view.findViewById(R.id.desc_txt);
+            title = (TextView) view.findViewById(R.id.title);
+            count = (TextView) view.findViewById(R.id.count);
+            thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
+            overflow = (ImageView) view.findViewById(R.id.overflow);
         }
     }
 
 
-    public TaskAdapter(List<Task> taskList, onBack ob) {
-        this.taskList = taskList;
-        this.ob=ob;
-
+    public AlbumsAdapter(Context mContext, List<Image> imageList) {
+        this.mContext = mContext;
+        this.imageList = imageList;
     }
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.task_row_item, parent, false);
+                .inflate(R.layout.recycler_item_layout, parent, false);
 
         return new MyViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
-        final Task task = taskList.get(position);
-        holder.task.setText(task.getTask());
-        holder.date.setText(task.getDate());
+    public void onBindViewHolder(final MyViewHolder holder, int position) {
+        Image image = imageList.get(position);
 
-        holder.task.setOnClickListener(new View.OnClickListener() {
+        // loading album cover using Glide library
+//        Glide.with(mContext).load(album.getThumbnail()).into(holder.thumbnail);
+
+        holder.overflow.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                ob.myclicklistener(task);
+            public void onClick(View view) {
+                showPopupMenu(holder.overflow);
             }
         });
     }
 
-    @Override
-    public int getItemCount() {
-        return taskList.size();
+    /**
+     * Showing popup menu when tapping on 3 dots
+     */
+    private void showPopupMenu(View view) {
+        // inflate menu
+        PopupMenu popup = new PopupMenu(mContext, view);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.menu_album, popup.getMenu());
+        popup.setOnMenuItemClickListener(new MyMenuItemClickListener());
+        popup.show();
     }
 
-    public interface onBack{
-        void myclicklistener( Task m);
+    /**
+     * Click listener for popup menu items
+     */
+    class MyMenuItemClickListener implements PopupMenu.OnMenuItemClickListener {
+
+        public MyMenuItemClickListener() {
+        }
+
+        @Override
+        public boolean onMenuItemClick(MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.action_add_favourite:
+                    Toast.makeText(mContext, "Add to favourite", Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.action_play_next:
+                    Toast.makeText(mContext, "Play next", Toast.LENGTH_SHORT).show();
+                    return true;
+                default:
+            }
+            return false;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return imageList.size();
     }
 }
